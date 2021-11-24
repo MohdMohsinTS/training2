bc36adc9d360d56d19a0ccb63259ae19ce585b87
Your Name <raomohsin776@gmail.com>
2021-11-24 19:59:07 +0530

    new 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,12 @@
 
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import Form from './Form'
 
 ReactDOM.render(
 
-    <App /> ,
+    <Form /> ,
   document.getElementById('root')
 );
 
